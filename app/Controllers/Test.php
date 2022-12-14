8425cb89f26a4b819968d9240534f00b7928e197
borzakap <borzakap@gmail.com>
2022-12-14 17:23:56 +0200

    set test

--- app/Controllers/Test.php
+++ app/Controllers/Test.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use Pipedrive;
 /**
  * Description of Test
  *
@@ -11,6 +12,21 @@ class Test extends BaseController{
     
     public function index()
     {
-        return view('welcome_message');
+        $pipedrive_conf = config('Pipedrive');
+        $this->pipedrive = new Pipedrive\Client($pipedrive_conf->oAuthClientId, $pipedrive_conf->oAuthClientSecret, $pipedrive_conf->oAuthRedirectUri);
+        // callback stores token for reuse when token is updated
+        Pipedrive\Configuration::$oAuthTokenUpdateCallback = function ($token) {
+            cache()->save("pipe_drive_oath_token", $token, 0);
+        };
+        // check if a token is available
+        if(!cache('pipe_drive_oath_token')){
+            // redirect user to a page that handles authorization
+            return redirect()->to(filter_var($pipedrive_conf->oAuthRedirectUri, FILTER_SANITIZE_URL));
+        }
+        // set access token in configuration
+        Pipedrive\Configuration::$oAuthToken = cache('pipe_drive_oath_token');
+        $pipedrive_user = $this->pipedrive->getUsers()->getCurrentUserData();
+        print_r($pipedrive_user);
+        die();
     }
 }

f09f22c69078952b313a6b92f0110a45ac7b6737
oleksiiborzak <oleksii.borzak@softrize.com>
2023-04-03 20:17:38 +0300

    fix a bag in snail about payments
    #AM-216

--- app/Views/payments/_form.php
+++ app/Views/payments/_form.php
@@ -1,11 +1,11 @@
 <div class="row">
     <div class="col-md-6 mt-3">
         <?= form_label(lang('Forms.Labels.ContragentName'), 'contragent_id', ['class' => 'form-label']) ?>
-        <?= form_dropdown('contragent_id', $contragents, $item->contragent_id, ['class' => 'form-select', 'id' => 'contragent_id']) ?>
+        <?= form_dropdown('contragent_id', $contragents, $item->contragent_id ?? null, ['class' => 'form-select', 'id' => 'contragent_id']) ?>
     </div>
     <div class="col-md-6 mt-3">
         <?= form_label(lang('Forms.Labels.CurrencyName'), 'currency_id', ['class' => 'form-label']) ?>
-        <?= form_dropdown('currency_id', $currencites, $item->currency_id, ['class' => 'form-select', 'id' => 'currency_id']) ?>
+        <?= form_dropdown('currency_id', $currencites, $item->currency_id ?? null, ['class' => 'form-select', 'id' => 'currency_id']) ?>
     </div>
     <div class="col-md-6 mt-3">
         <?= form_label(lang('Forms.Labels.Summ'), 'summ', ['class' => 'form-label']) ?>
@@ -13,6 +13,6 @@
     </div>
     <div class="col-md-6 mt-3">
         <?= form_label(lang('Forms.Labels.PaymentAt'), 'payment_at', ['class' => 'form-label']) ?>
-        <?= form_input(['name' => 'payment_at', 'class' => 'form-control', 'id' => 'payment_at', 'type' => 'date', 'value' => old('payment_at') ?? $item->payment_at->toDateString() ?? '']) ?>
+        <?= form_input(['name' => 'payment_at', 'class' => 'form-control', 'id' => 'payment_at', 'type' => 'date', 'value' => old('payment_at') ?? (isset($item->payment_at) ? $item->payment_at->toDateString() : null) ?? '']) ?>
     </div>
 </div>
